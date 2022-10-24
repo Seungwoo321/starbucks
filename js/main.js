12a4f8703a08b3e4e625cc9f3aadd15369681c17
Seungwoo321 <seungwoo321@gmail.com>
2022-10-24 20:33:33 +0900

    study: 2022.10.24

--- js/main.js
+++ js/main.js
@@ -34,3 +34,38 @@ window.addEventListener('scroll', _.throttle(function () {
     }
 }, 300));
 // _.throttle(함수, ms단위 시간)
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+    gsap.to(fadeEl, 1, {
+        delay: (index + 1) * .7,
+        opacity: 1
+    });
+});
+
+new Swiper('.notice-line .swiper-container', {
+    direction: 'vertical',
+    autoplay: true,
+    loop: true
+});
+
+new Swiper('.promotion .swiper-container', {
+    slidesPerView: 3, // 한번에 보여줄 슬라이드 개수,
+    spaceBetween: 10, // 슬라이드 사이 여백
+    centeredSlides: true, // 1번 슬라이드가 가운데 정렬
+    loop: true,
+    // autoplay: {
+    //     delay: 5000
+    // }
+    pagination: {
+        el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+        clickable: true
+    },
+    navigation: {
+        prevEl: '.promotion .swiper-prev',
+        nextEl: '.promotion .swiper-next'
+    }
+});
+
+const promotionEl = document.querySelector('.promotion')
+const promotionToggleBtn = document.querySelector('.toggle-promotion')
